3db5088f79ebca21adb142b488d83e7abc75fa69
Susith Deshan Alwis <iamsusithalwis@gmail.com>
2024-10-27 17:03:45 +0530

    Image Upload Section improvemnts

--- static/script.js
+++ static/script.js
@@ -32,3 +32,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+function previewImage(event, previewId) {
+    const input = event.target;
+    const preview = document.getElementById(previewId);
+
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            preview.src = e.target.result;
+            preview.style.display = 'block'; // Show the preview
+        };
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        preview.src = '';
+        preview.style.display = 'none'; // Hide the preview if no file is selected
+    }
+}
